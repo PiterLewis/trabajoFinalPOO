6c058017464c92ab2d1cff499b731c50bc95305f
jgonzalezhub <160738734+jgonzalezhub@users.noreply.github.com>
2024-12-13 13:07:17 +0100

    Update Medico.java

--- Medico.java
+++ Medico.java
@@ -1,14 +1,1 @@
-package BattleTerminal;
 
-public class Medico extends Jugador {
-    // Constructor
-    public Medico(String nombre, Coordenada posicion) {
-        super(120, 15, 40, posicion, nombre);
-    }
-
-    // Implementación de la habilidad especial: Curación rápida
-    @Override
-    public void usarHabilidad() {
-        System.out.println("El médico usa su curación rápida para restaurar vida.");
-    }
-}
