fdb8f3b32e66d5ec4cd6a77062bfec289a8cb88b
ch0rtas <47576091+ch0rtas@users.noreply.github.com>
2024-12-14 21:03:38 +0100

    Update compatibilidad CuracionRapida

--- Jugador.java
+++ Jugador.java
@@ -3,16 +3,18 @@ class Jugador extends Entidad {
     private String personaje;
     private Arma arma;
     private Boolean habilidadEspecialDisponible;
+    private int vidaMaxima;
 
     public Jugador(String nombre, Integer vida, Integer[] posicion, String personaje) {
         super(nombre, vida, posicion);
         this.personaje = personaje;
         this.habilidadEspecialDisponible = true;
+        this.vidaMaxima = vida; // La vida inicial también es la máxima
     }
 
-    public void usarHabilidadEspecial() {
+    public void usarHabilidadEspecial(HabilidadEspecial habilidad) {
         if (habilidadEspecialDisponible) {
-            System.out.println("Habilidad especial usada: " + personaje);
+            habilidad.activar(this);
             habilidadEspecialDisponible = false;
         } else {
             System.out.println("Habilidad especial no disponible.");
@@ -29,5 +31,13 @@ class Jugador extends Entidad {
 
     public String getPersonaje() {
         return personaje;
+    }
+
+    public int getVidaMaxima() {
+        return vidaMaxima;
+    }
+
+    public void setHabilidadEspecialDisponible(Boolean disponible) {
+        this.habilidadEspecialDisponible = disponible;
     }
 }
